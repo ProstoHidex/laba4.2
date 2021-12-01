dfed6603e79320cece561c92a19bfe3334dec4db
Hidex <seryga38120@mail.ru>
2021-12-01 23:49:02 +0700

    Создание программы с полным функционалом

--- Laba4.1/Laba4.1/Program.cs
+++ Laba4.1/Laba4.1/Program.cs
@@ -4,11 +4,71 @@ namespace Laba4._1
 {
     class Program
     {
+        private static double A, B;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+
+
+            while (true)
+            {
+                Console.WriteLine("          MENU            ");
+                Console.WriteLine("1. Ввести A");
+                Console.WriteLine("2. Ввести B");
+                Console.WriteLine("3. Выполнить операцию «+»");
+                Console.WriteLine("4. Выполнить операцию «-»");
+                Console.WriteLine("5. Выполнить операцию «*»");
+                Console.WriteLine("6. Выполнить операцию «/»");
+                Console.WriteLine("Выберите пункт меню");
+                char ch = char.Parse(Console.ReadLine());
+                switch (ch)
+                {
+                    case '1':
+                        InputA(Console.ReadLine());
+                        break;
+                    case '2':
+                        InputB(Console.ReadLine());
+                        break;
+                    case '3':
+                        Plus(A, B);
+                        break;
+                    case '4':
+                        Minus(A, B);
+                        break;
+                    case '5':
+                        Mult(A, B);
+                        break;
+                    case '6':
+                        Del(A, B);
+                        break;
+                    default:
+                        Console.WriteLine("Произошла ошибка, введите заного");
+                        break;
+                }
+            }
+            
+            static void InputA(string input)
+            {
+
+
+                A = double.Parse(input);
+
+
+            }
+            static void InputB(string input)
+            {
+
+
+                B = double.Parse(input);
+
+
+            }
+            static void Plus(double a, double b) => Console.WriteLine(a + b);
+
+            static void Minus(double a, double b) => Console.WriteLine(a - b);
+
+            static void Mult(double a, double b) => Console.WriteLine(a * b);
+
+            static void Del(double a, double b) => Console.WriteLine(a / b);
         }
     }
 }
